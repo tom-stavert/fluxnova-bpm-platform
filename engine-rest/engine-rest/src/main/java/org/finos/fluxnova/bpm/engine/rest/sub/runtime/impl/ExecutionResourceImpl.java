1820caff640bd1d8e8acb2f73b933235bcea789f
Tom Stavert <tstavert@scottlogic.com>
2026-04-08 09:59:24 +0100

    Add REST endpoints for ad-hoc subprocess interaction (Layer 5 / fn-81s)
    Two new endpoints on the execution sub-resource:
    
      POST /execution/{id}/trigger-ad-hoc-activity
        Body: { "activityId": "..." }
        Calls RuntimeService.triggerAdHocActivity(); maps BadUserRequestException
        to 400, ProcessEngineException to 500, AuthorizationException rethrown.
    
      POST /execution/{id}/complete-ad-hoc
        No body.
        Calls RuntimeService.completeAdHocSubprocess(); same exception mapping.
    
    - TriggerAdHocActivityDto: simple single-field DTO for the request body

--- engine-rest/engine-rest/src/main/java/org/finos/fluxnova/bpm/engine/rest/sub/runtime/impl/ExecutionResourceImpl.java
+++ engine-rest/engine-rest/src/main/java/org/finos/fluxnova/bpm/engine/rest/sub/runtime/impl/ExecutionResourceImpl.java
@@ -28,6 +28,7 @@ import org.finos.fluxnova.bpm.engine.rest.dto.VariableValueDto;
 import org.finos.fluxnova.bpm.engine.rest.dto.runtime.ExecutionDto;
 import org.finos.fluxnova.bpm.engine.rest.dto.runtime.ExecutionTriggerDto;
 import org.finos.fluxnova.bpm.engine.rest.dto.runtime.IncidentDto;
+import org.finos.fluxnova.bpm.engine.rest.dto.runtime.TriggerAdHocActivityDto;
 import org.finos.fluxnova.bpm.engine.rest.exception.InvalidRequestException;
 import org.finos.fluxnova.bpm.engine.rest.exception.RestException;
 import org.finos.fluxnova.bpm.engine.rest.sub.VariableResource;
@@ -91,6 +92,32 @@ public class ExecutionResourceImpl implements ExecutionResource {
   @Override
   public EventSubscriptionResource getMessageEventSubscription(String messageName) {
     return new MessageEventSubscriptionResource(engine, executionId, messageName, objectMapper);
+  }
+
+  @Override
+  public void triggerAdHocActivity(TriggerAdHocActivityDto dto) {
+    try {
+      engine.getRuntimeService().triggerAdHocActivity(executionId, dto.getActivityId());
+    } catch (AuthorizationException e) {
+      throw e;
+    } catch (BadUserRequestException e) {
+      throw new InvalidRequestException(Status.BAD_REQUEST, e, e.getMessage());
+    } catch (ProcessEngineException e) {
+      throw new RestException(Status.INTERNAL_SERVER_ERROR, e, e.getMessage());
+    }
+  }
+
+  @Override
+  public void completeAdHoc() {
+    try {
+      engine.getRuntimeService().completeAdHocSubprocess(executionId);
+    } catch (AuthorizationException e) {
+      throw e;
+    } catch (BadUserRequestException e) {
+      throw new InvalidRequestException(Status.BAD_REQUEST, e, e.getMessage());
+    } catch (ProcessEngineException e) {
+      throw new RestException(Status.INTERNAL_SERVER_ERROR, e, e.getMessage());
+    }
   }
 
   @Override
